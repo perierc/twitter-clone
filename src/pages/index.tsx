2411cae15b28fbeae54fecac5194d89e0e420806
Charles Perier <charles@upflowy.com>
2023-04-01 22:10:59 +1100

    create Post model and posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,11 +17,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+        <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
